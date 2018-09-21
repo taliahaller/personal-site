2598b29f64fdf01fc50deaef621d29c5ca009eb9
Student <example@example.com>
2018-09-21 10:53:13 -0700

    add CSS Styling.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import './sass/styles.scss';
+import $ from 'jquery';
+
+
+$(document).ready(function(){
+
+  $(window).scroll(function(){
+   if ($(this).scrollTop() < 150) {
+     $(".navBar").removeClass("scrolled");
+   }
+  });
+
+  $(window).scroll(function(){
+   if ($(this).scrollTop() > 150) {
+     $(".navBar").addClass("scrolled");
+   }
+ });
+
+}); /*Document Ready Close*/
