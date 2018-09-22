0af520cc59d85d40842256014a5e3354add669d5
Student <example@example.com>
2018-09-21 17:55:30 -0700

    add JS animations.

--- src/index.js
+++ src/index.js
@@ -16,4 +16,18 @@ $(document).ready(function(){
    }
  });
 
+
+ $(window).scroll(function(){
+  if ($(this).scrollTop() > 2700) {
+    $(".insideBanner").addClass("animated bounce");
+  }
+});
+
+$(window).scroll(function(){
+ if ($(this).scrollTop() < 2700) {
+   $(".insideBanner").removeClass("animated bounce");
+ }
+});
+
+
 }); /*Document Ready Close*/
